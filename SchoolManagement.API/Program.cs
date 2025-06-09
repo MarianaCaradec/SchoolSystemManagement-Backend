eb9d9a3d67a7c01466522b60b2267657c454d867
Mariana Caradec <maricaradec@gmail.com>
2025-06-09 17:37:21 -0300

    feat: added cors to connect from React's frontend

--- SchoolManagement.API/Program.cs
+++ SchoolManagement.API/Program.cs
@@ -62,7 +62,16 @@ builder.Services.AddAuthentication(options =>
 
 builder.Services.AddAuthorization();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 var app = builder.Build();
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

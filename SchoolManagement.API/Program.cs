53e5555e919f7b7146eec107ea60bd7b14008a72
Mariana Caradec <maricaradec@gmail.com>
2025-06-30 20:37:46 -0300

    fix: fixed wrong change in CORS permission

--- SchoolManagement.API/Program.cs
+++ SchoolManagement.API/Program.cs
@@ -71,7 +71,7 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-app.UseCors("AllowFrontend");
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

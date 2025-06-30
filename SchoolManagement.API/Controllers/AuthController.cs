659d2a5b6ce3b8530243dda37d9df1634db97f89
Mariana Caradec <maricaradec@gmail.com>
2025-06-30 19:57:57 -0300

    fix: added directly cookies creation in login method and removed useless auth method

--- SchoolManagement.API/Controllers/AuthController.cs
+++ SchoolManagement.API/Controllers/AuthController.cs
@@ -10,14 +10,39 @@ namespace SchoolManagement.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController(IAuthService authService) : ControllerBase
+    public class AuthController(IAuthService authService, IUserService userService) : ControllerBase
     {
         private readonly IAuthService _authService = authService;
+        private readonly IUserService _userService = userService;
 
-        [HttpPost("Authenticate")]
-        public async Task<ActionResult<string>> Authenticate([FromBody] AuthReq req)
+        //[HttpPost("Authenticate")]
+        //public async Task<ActionResult<string>> Authenticate([FromBody] AuthReq req)
+        //{
+        //    string token = await _authService.AuthenticateAsync(req.Email, req.Password, req.UserId);
+
+        //    var cookies = new CookieOptions
+        //    {
+        //        HttpOnly = true,
+        //        Secure = true,
+        //        SameSite = SameSiteMode.Strict,
+        //        Expires = DateTime.UtcNow.AddMinutes(60)
+        //    };
+
+        //    Response.Cookies.Append("AuthToken", token, cookies);
+
+        //    return Ok(new { Message = "Authentication successful" });
+        //}
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDto>> Register([FromBody] Auth authUser, [FromQuery] int? userId)
         {
-            string token = await _authService.AuthenticateAsync(req.Email, req.Password, req.UserId);
+            return await _authService.RegisterAsync(authUser, userId);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<AuthDto>> Login([FromBody] AuthReq req)
+        {
+            AuthDto token = await _authService.LoginAsync(req.Email, req.Password, req.UserId);
 
             var cookies = new CookieOptions
             {
@@ -27,21 +52,11 @@ namespace SchoolManagement.API.Controllers
                 Expires = DateTime.UtcNow.AddMinutes(60)
             };
 
-            Response.Cookies.Append("AuthToken", token, cookies);
+            Response.Cookies.Append("AuthToken", token.ToString(), cookies);
 
-            return Ok(new { Message = "Authentication successful" });
-        }
+            UserDto user = await _userService.GetUserByEmailAsync(req.Email, req.UserId);
 
-        [HttpPost("Register")]
-        public async Task<ActionResult<UserDto>> Register([FromBody] Auth authUser, [FromQuery] int? userId)
-        {
-            return await _authService.RegisterAsync(authUser, userId);
-        }
-
-        [HttpPost("Login")]
-        public async Task<ActionResult<AuthDto>> Login([FromBody] LoginReq req)
-        {
-            return await _authService.LoginAsync(req.Email, req.Password);
+            return Ok(new AuthDto(user.Email, user.Role));
         }   
     }
 }
